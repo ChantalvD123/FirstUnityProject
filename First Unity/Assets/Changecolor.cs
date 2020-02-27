759ac6b1e51eb9b227ca520328fa3652144c1360
chantalvd <chantal.van.dinten@hotmail.nl>
2020-02-27 13:02:43 +0100

    UnityProject goede

---
+++ First Unity/Assets/Changecolor.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class Changecolor : MonoBehaviour
+{
+    public MeshRenderer mr;
+    void OnCollisionEnter(UnityEngine.Collision collisionInfo)
+    {
+        if (collisionInfo.gameObject.tag == "MainCamera")
+        {
+            Debug.Log("hit");
+            
+
+        }
+    }
+}
