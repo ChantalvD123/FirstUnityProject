e662e312b73b64d89b27a2df82700fcfcf905db1
chantalvd <chantal.van.dinten@hotmail.nl>
2020-03-26 12:03:39 +0100

    Tweede upload BackUp voor verranderingen

---
+++ HKU_FP_Game/Assets/RW/CoinScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    public AudioSource collectSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 90 * Time.deltaTime, 0); 
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        if (other.name == "Player")
+        {
+            collectSound.Play();
+            other.GetComponent<PlayerScript>().points++;
+            //Add 1 to points
+            Destroy(gameObject); //this destroys things
+            Debug.Log("trigger");
+        }
+    }
+}
---
+++ HKU_FP_Game/Assets/RW/PlayerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public int points = 0;
+    GUIStyle style = new GUIStyle();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        this.style.fontSize = 100;
+        this.style.normal.textColor = Color.white;
+        GUI.Label(new Rect(10,10,100,20), "Score : " + points, style);
+    }
+}
